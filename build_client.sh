bac184b5faa7730ceebce17d7dfaa0ea47e0bb76
Josh Fried <joshuafried@gmail.com>
2020-09-04 11:24:08 -0400

    client build

--- build_client.sh
+++ build_client.sh
@@ -8,25 +8,45 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 echo "BASE_DIR = '${SCRIPTPATH}/'" > base_dir.py
 
+CORES=`getconf _NPROCESSORS_ONLN`
+
 git submodule update --init -f --recursive shenango
 
 if lspci | grep -q 'ConnectX-3'; then
   sed "s/CONFIG_MLX4=.*/CONFIG_MLX4=y/g" -i shenango/build/config
-elif lspci | grep -q 'ConnectX-5'; then
+elif lspci | grep -q 'ConnectX-[4,5]'; then
   for config in MLX5 DIRECTPATH; do
     sed "s/CONFIG_${config}=.*/CONFIG_${config}=y/g" -i shenango/build/config
   done
 fi
 
+echo building DPDK
 pushd shenango
-build/init_submodules.sh
-popd
+patch -p 1 -d dpdk/ < build/ixgbe_19_11.patch
+if lspci | grep -q 'ConnectX-[4,5]'; then
+  patch -p 1 -d dpdk/ < build/mlx5_19_11.patch
+elif lspci | grep -q 'ConnectX-3'; then
+  patch -p 1 -d dpdk/ < build/mlx4_19_11.patch
+fi
+make -C dpdk/ config T=x86_64-native-linuxapp-gcc
+make -C dpdk/ -j $CORES
 
-pushd shenango/ksched
+if lspci | grep -q 'ConnectX-5'; then
+echo building RDMA-CORE
+pushd rdma-core
+git apply ../build/rdma-core.patch
+EXTRA_CMAKE_FLAGS=-DENABLE_STATIC=1 MAKEFLAGS=-j$CORES ./build.sh
+popd
+fi
+
+
+pushd ksched
 make
 popd
 
 echo building LOADGEN
-pushd shenango/apps/synthetic
+pushd apps/synthetic
 cargo build --release
+popd
+
 popd

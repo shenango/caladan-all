a84a6bad738812dcacba189d89ed4e250be1fcb7
Josh Fried <joshuafried@gmail.com>
2020-09-04 15:46:59 -0400

    scripts

--- graph_timeseries.py
+++ graph_timeseries.py
@@ -8,10 +8,6 @@ if sys.version_info[0] < 3:
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
-fig = plt.figure()
-
-gs = fig.add_gridspec(4, 4)
-axs = [fig.add_subplot(gs[i, :4]) for i in range(4)]
 
 cycles_per_us = None
 
@@ -23,17 +19,17 @@ def readfile(f):
 
     is_sorted = True
     last_i = 0
+    print("Reading full request trace... (may take awhile)")
     for line in d.splitlines():
-        if "ticks / us" in l:
+        if "ticks / us" in line:
             global cycles_per_us
-            cycles_per_us = int(l.split("detected")[1].split()[0])
+            cycles_per_us = int(line.split("detected")[1].split()[0])
             continue
 
         line = line.split("Trace: ")
         if len(line) < 2:
             continue
 
-        print("Reading full request trace... (may take awhile)")
         for tup in line[1].split()[:-1]:
             a, b, c = tup.split(":", 2)
             if ':' in c:
@@ -89,7 +85,7 @@ def windower(x, y, cycles_per_us, ms_window=20, percentile=0.999):
             start = i
     return newx, newy, newz
 
-def read_mem(directory, first_tsc, last_tsc, cycles_per_us):
+def read_mem(directory, first_tsc, last_tsc, cycles_per_us, ax):
     x, y = [], []
     with open("{}/mem.log".format(directory)) as f:
         for line in f:
@@ -102,16 +98,18 @@ def read_mem(directory, first_tsc, last_tsc, cycles_per_us):
             except:
                 continue
     x, y, _z = windower(x, y, cycles_per_us, percentile='avg')
-    axs[2].plot(x,y)
-    axs[2].set_ylabel("Mem BW\n(MB/s)")
+    ax.plot(x,y)
+    ax.set_ylabel("Mem BW\n(MB/s)")
 
-def parse_shmlog(arg, first_tsc, last_tsc, cycles_per_us):
+def parse_shmlog(arg, first_tsc, last_tsc, cycles_per_us, ax):
     xs, ys = [], []
     lastx = None
     for l in open(arg):
         try:
-            ls = list(map(int, l.strip().split()))
-        except:
+            ls = l.strip().split()
+            hxkey = int(ls[0], 16)
+            ls = list(map(int, ls[1:]))
+        except Exception as e:
             continue
 
         if ls[-1] < first_tsc: continue
@@ -130,13 +128,26 @@ def parse_shmlog(arg, first_tsc, last_tsc, cycles_per_us):
         cycles_per_s = cycles_per_us * float(1e6)
         # ops / cycle, cycles per us
         ys.append(cycles_per_s / cycles_per_op)
+    assert xs, arg
     xs, ys, _z = windower(xs, ys, cycles_per_us, percentile='avg')
     lbl = arg.split("/")[-1].split("_shm_query")[0]
-    axs[3].plot(xs, ys, label=lbl)
-    axs[3].set_ylabel("BE Tput\n(Op/s)")
-    axs[3].legend()
+
+    max_bes = {
+        'x264': 56.8336,
+        'streamcluster': 926049,
+        'swaptionsGC': 2326233.0 / 142.0,
+        'streamDRAM': 1945.0,
+    }
+    ys = list(map(lambda a: a * 100.0 / max_bes[lbl], ys))
+    ax.plot(xs, ys, label=lbl)
+    ax.set_ylabel("BE Op/s\n(%)")
+    ax.legend()
 
 def graph_experiment_figure6(directory):
+    plt.clf()
+    fig = plt.figure()
+    gs = fig.add_gridspec(4, 4)
+    axs = [fig.add_subplot(gs[i, :4]) for i in range(4)]
     earliest_ts, latest_ts = 0, 0
     files = os.listdir(directory)
     with open("{}/memcached.out".format(directory)) as f:
@@ -160,8 +171,8 @@ def graph_experiment_figure6(directory):
             latest_ts = tm_tsc[-1]
     axs[1].legend()
     axs[0].legend()
-    read_mem(directory, earliest_ts, latest_ts, cycles_per_us)
-    parse_shmlog("{}/swaptionsGC_shm_query.out".format(directory), earliest_ts, latest_ts, cycles_per_us)
+    read_mem(directory, earliest_ts, latest_ts, cycles_per_us, axs[2])
+    parse_shmlog("{}/swaptionsGC_shm_query.out".format(directory), earliest_ts, latest_ts, cycles_per_us, axs[3])
     plt.xlabel("Time (s)")
     w, h = fig.get_size_inches()
     fig.set_size_inches(w*1.5, h*1.5)
@@ -169,31 +180,46 @@ def graph_experiment_figure6(directory):
     plt.savefig("figure_6_caladan.pdf")
 
 def graph_experiment_figure8(directory):
+    fig = plt.figure()
+    gs = fig.add_gridspec(5, 5)
+    axs = [fig.add_subplot(gs[i, :5]) for i in range(5)]
     earliest_ts, latest_ts = 0, 0
     files = os.listdir(directory)
-    dats = []
+    dats = {}
     memcs = []
     for f in files:
         if f.endswith(".memcached.out"):
             memcs.append(readfile(directory + "/" + f))
         elif f.endswith(".silo.out"):
-            dats['silo'] = zip(*readfile(directory + "/" + f))
+            dats['silo'] = list(zip(*readfile(directory + "/" + f)))
         elif f.endswith(".storage.out"):
-            dats['storage'] = zip(*readfile(directory + "/" + f))
-    dats['memcached'] = zip(*list(merge(*memcs)))
+            dats['storage'] = list(zip(*readfile(directory + "/" + f)))
+    dats['memcached'] = list(zip(*list(merge(*memcs))))
     min_tsc = min(dats[app][0][0] for app in dats)
     max_tsc = max(dats[app][0][-1] for app in dats)
-    for app, dat in dats.items():
+    maxs = {
+        'memcached': 10.0,
+        'silo': 0.5,
+        'storage': 0.293979,
+    }
+    for i, (app, dat) in enumerate(dats.items()):
         x,y,z = windower(dat[0], dat[2], cycles_per_us)
         print(len(x), len(y), len(z))
-        axs[0].plot(x,y, label=app)
-        axs[0].set_ylabel("99.9% Lat. (us)")
-        axs[1].plot(x,z, label=app)
-        axs[1].set_ylabel("LC\nThroughput")
-        axs[1].set_ylim(0, 1.25 * max(z))
-    axs[1].legend()
-    axs[0].legend()
-    read_mem(directory, min_tsc, max_tsc, cycles_per_us)
-    
+        axs[i].plot(x,y, label=app)
+        axs[i].set_ylabel("99.9% Lat. (us)")
+        z = [a * 100.0 / (1e6 * maxs[app]) for a in z]
+        axs[3].plot(x, z, label=app)
+        axs[i].legend()
+        axs[3].set_ylabel("LC\nThroughput (%)")
+        axs[3].set_ylim(0, 1.25 * max(z))
+    axs[3].legend()
+    for be in ["swaptionsGC", "streamcluster"]:
+        parse_shmlog("{}/{}_shm_query.out".format(directory, be), min_tsc, max_tsc, cycles_per_us, axs[4])
+    plt.xlabel("Time (s)")
+    w, h = fig.get_size_inches()
+    fig.set_size_inches(w*1.5, h*1.5)
+    fig.tight_layout()
+    plt.savefig("figure_8_multiapp.pdf")
+
 
 
